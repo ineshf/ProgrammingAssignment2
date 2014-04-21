9a79f51a05462908956876cb7261e2dbc76f694d
Ines Huertas <ines.huertasd@gmail.com>
2014-04-21 21:44:16 +0200

    Ines Huertas commit R programming: Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function makeCacheMatrix(x=matrix())
+## This function creates a special "matrix", which is really a list containing
+#a function to:
+# set the value of matrix
+# get the value of matrix
+# set the value of the inverse matrix
+# get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # init matrix default
+  matrix <- NULL
+  
+  # set function set matrix value
+  set <- function(newmatrix) {
+    x <<- newmatrix
+    matrix<<- NULL
+  }
+  
+  # get function get matrix value
+  get <- function() x
+  
+  # setmatrix function set matrix inverse values
+  setmatrix <- function(matrix_inverse) matrix <<- matrix_inverse
+  
+  # get matrix function get matrix inverse values
+  getmatrix <- function() matrix
+  
+  # create list with the params of matrix
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Function cacheSolve(x,...)
+# x is matrix to calculates it inverse matrix
+# return the inverse matrix of x
+#The following function calculates the inverse matrix of the special "matrix" created with the above function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  # Check if the matrix is in cache
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    
+        message("getting cached inverse matrix")
+        #First checks to see if the mean has already been calculated. If so, it gets the inverse from the cache 
+        #and skips the computation.
+        # Return the inverse matrix stored in cache
+        return(m)
+      }
+  
+      #It calculates the inverse matrix of the data and sets the value of
+      #the inverse matrix in the cache via the setmatrix function.
+  
+      data <-x$get()
+      m <- solve(data)
+      x$setmatrix(m)
+  
+  # Return de inverse matrix calculated
+  m
 }
